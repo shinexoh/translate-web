7cc97ca09f66eed408f2721a0fb786f8fa0f1ef5
Shinex <shinexoh@qq.com>
2024-09-19 17:01:00 +0800

    Fix file import error

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { ThemeProvider } from './components/ThemeProvider.tsx';
+import { ThemeProvider } from './components/theme-provider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <ThemeProvider>
